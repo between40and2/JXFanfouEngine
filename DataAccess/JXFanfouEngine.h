cd58b5a643a45237c1dd7f8e6c28ee7015980ae1
Juguang XIAO <juguang@gmail.com>
2011-01-11 18:37:42 +0800

    second commit

---
+++ DataAccess/JXFanfouEngine.h
@@ -0,0 +1,16 @@
+//
+//  JXFanfouEngine.h
+//  JXFanfouEngine
+//
+//  Created by Juguang XIAO on 1/11/11.
+//  Copyright 2011 PostBabel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface JXFanfouEngine : NSObject {
+
+}
+
+@end
---
+++ DataAccess/JXFanfouEngine.m
@@ -0,0 +1,14 @@
+//
+//  JXFanfouEngine.m
+//  JXFanfouEngine
+//
+//  Created by Juguang XIAO on 1/11/11.
+//  Copyright 2011 PostBabel. All rights reserved.
+//
+
+#import "JXFanfouEngine.h"
+
+
+@implementation JXFanfouEngine
+
+@end
